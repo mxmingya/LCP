7439e51f982e8dd6fb6fd69c1f2ad092deb9716e
maxxx950 <maxxx950@umn.edu>
2018-05-23 01:22:34 -0400

    similar to number of stairs, such that dp[i] = cost[i] + min(dp[i-1]+dp[i-2]) which is, the current cost is based on lower one from one step away and two steps away

---
+++ 746MinCostClimbingStairs.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        if not cost or len(cost) == 0:
+            return 0
+        
+        l = len(cost)
+        if l == 1:
+            return cost[0]
+        
+        step2, step1 = cost[0], cost[1]
+        
+        for i in range(2, l):
+            temp = step1
+            step1 = cost[i] + min(step1, step2)
+            step2 = temp
+            
+        
+        return min(step1, step2)
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        if not cost or len(cost) == 0:
+            return 0
+        
+        l = len(cost)
+        if l == 1:
+            return cost[0]
+        
+        dp = [cost[0], cost[1]]
+        
+        
+        for i in range(2, l):
+            dp.append(cost[i] + min(dp[i-1], dp[i-2]))
+        
+        return min(dp[-1], dp[-2])
