cdc92e83ca0a08fea450bf255ff458b9ef4c173a
maxxx950 <maxxx950@umn.edu>
2018-05-29 15:25:25 -0400

    flip the row, then flip the bit

--- 832FlipImages.py
+++ 832FlipImages.py
@@ -10,8 +10,23 @@ class Solution(object):
                 temp = arr[i]
                 arr[i] = arr[l-i-1]
                 arr[l-i-1] = temp
-            return map(lambda n: 1-n, arr)
+            return map(lambda n: 1-n, arr.reverse())
         for i in range(len(A)):
             A[i] = flip(A[i])
+            
+        return A
+
+#a more neat version
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        l, cl = len(A), len(A[0])
+        
+        for i in range(l):
+            A[i] = map(lambda x: 1-x, A[i])
+            A[i].reverse()
             
         return A
